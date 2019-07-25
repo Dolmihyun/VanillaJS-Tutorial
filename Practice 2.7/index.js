54749d8fa1944ebda405606312c23b196ad62b17
dolmi <dolmihyun@gmail.com>
2019-07-25 14:40:47 +0900

    practices done
    toggle

--- Practice 2.7/index.js
+++ Practice 2.7/index.js
@@ -1,17 +1,32 @@
 const title = document.querySelector("#title");
+const title2 = document.querySelector("#title_toggle");
 
 const CLICKED_CLASS = "clicked";
 
+// function handleClick() {
+//   const currentClass = title.className;
+//   if (currentClass !== CLICKED_CLASS) {
+//     title.className = CLICKED_CLASS;
+//   } else {
+//     title.className = "";
+//   }
+// }
+
 function handleClick() {
-  const currentClass = title.className;
-  if (currentClass !== CLICKED_CLASS) {
-    title.className = CLICKED_CLASS;
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (hasClass) {
+    title.classList.remove(CLICKED_CLASS);
   } else {
-    title.className = "";
+    title.classList.add(CLICKED_CLASS);
   }
+}
+
+function handleToggle() {
+  title2.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
   title.addEventListener("click", handleClick);
+  title2.addEventListener("click", handleToggle);
 }
 init();
